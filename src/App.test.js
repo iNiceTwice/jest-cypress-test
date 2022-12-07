e7562453e76c22b0f685e14b27f2deacb248b292
NiceTwice <onlynicetwice@gmail.com>
2022-12-07 15:20:07 -0300

    added tailwind config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, getByText } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import React from "react"
 import App from './App';
+import { Contador } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const element = screen.getByText(/anashe/);
-  expect(element).toBeInTheDocument();
-});
+
+test("verifica que se muestre el div al tocar el boton", () => {
+  render(<App/>)
+  const button = screen.getByText("Mostrar")
+  fireEvent.click(button)
+  const element = screen.getByText("Soy visible!")
+  expect(element).toBeInTheDocument()
+})
+
+describe("<Counter/>", ()=>{
+  
+  beforeEach(()=>{
+    render(<Contador/>)
+  })
+  
+  test('verifica que el contador sume', () => {
+    const button = screen.getByText("+")
+    fireEvent.click(button)
+    const element = screen.getByText("1");
+    expect(element).toBeInTheDocument();
+  });
+  
+  test("si counter es > a 3 deberia ser rojo", () => {
+    const button = screen.getByText("+")
+    for(let i = 0 ; i < 4 ; i++){
+      fireEvent.click(button)
+    }
+    const element = screen.getByText("4")
+    expect(element).toHaveStyle("color:red")
+  })
+})
